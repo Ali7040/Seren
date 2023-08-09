dea9110e88baeda46e033bb0a14e332ca9367da6
Ali Haider <ha4219466@gmail.com>
2023-08-09 08:55:26 -0700

    Add new component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css';
 import  Hero  from './components/Hero.jsx';
 import TopMenu from './components/TopMenu.jsx';
+import Main from './components/Main';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <div className=" bg-hero-bg bg-cover bg-no-repeat bg-center">
         <TopMenu />
         <Hero />
-      
+        <Main />
       </div>
     </div>
   );
